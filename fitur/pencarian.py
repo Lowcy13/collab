3e85c880944bab4231bd3c50e8ff567e3da7e706
hikma-rmd <60900123009@uin-alauddin.ac.id>
2025-07-04 02:52:08 +0800

    Tambah fitur pencarian berdasarkan kategori

--- fitur/pencarian.py
+++ fitur/pencarian.py
@@ -1,8 +1,6 @@
-# fitur/pencarian.py
-# Berisi fungsi pencarian berdasarkan kategori
-
 from data.produk import daftar_produk
 
 def cari_produk_berdasarkan_kategori(kategori):
+    print(f"[LOG] Mencari produk kategori: {kategori}")
     hasil = [p for p in daftar_produk if p["kategori"].lower() == kategori.lower()]
     return hasil
